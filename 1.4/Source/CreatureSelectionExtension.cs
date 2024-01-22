8f8bf6a17d32aeab655e9d495b8d6e1b018bc4aa
Mr Samuel Streamer <59952292+Roll1D2Games@users.noreply.github.com>
2024-01-22 20:05:35 +0000

    Delete 1.4/Source directory

--- 1.4/Source/CreatureSelectionExtension.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using Verse;
-
-namespace MrSamuelStreamer.RPGAdventureFlavourPack;
-
-public class CreatureSelectionExtension : DefModExtension
-{
-    public List<PawnKindDef> possiblePawnKinds;
-}
